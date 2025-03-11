ac31b25651259b9ab68692c41f3fe5b9edca45a2
shpachela <spyrkovich.dima@gmail.com>
2025-03-11 15:59:06 +0300

    finish create button

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
 import Header from "./components/Header";
+import WayToTeach from "./components/WayToTeach";
+import {ways, differences} from "./data";
+import Button from "./components/Button/Button";
+import {useState} from "react";
+
+
 
 export default function App() {
+  const [ContentTyape, setContentType ] = useState(null)
+  function handlCliclk (type) {
+    setContentType(type)
+}
+
   return (
     <div>
       <Header />
-
       <main>
         <section>
-          <h3>Наш подход к обучению</h3>
+          <h3>Наш подход к обучению </h3>
           <ul>
-            <li>
-              <p>
-                <strong>Фильтрация информации и технологий </strong> из
-                огромного огромного количества мноврмацтт т навыков в вы
-                получите только то что необходимо и работает
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Формат обучения</strong> из
-                огромного огромного количества мноврмацтт т навыков в вы
-                получите только то что необходимо и работает
-                {/* 49 min 58 sec */}
-                
-              </p>
-            </li>
+            <WayToTeach {...ways[0]}/>
+            <WayToTeach {...ways[1]}/>
+            <WayToTeach {...ways[2]}/>
+            <WayToTeach {...ways[3]}/>
           </ul>
+        </section>
+        <section>
+          <h3>Чем мы отличаемся от других</h3>
+
+          <Button isActive={ContentTyape === 'way'} onClick={() => handlCliclk('way')}>Подход</Button>
+          <Button isActive={ContentTyape === 'easy'} onClick={() => handlCliclk('easy')}>Доступность</Button>
+          <Button isActive={ContentTyape === 'program'} onClick={() => handlCliclk('program')}>Концентрация</Button>
+
+          {/* {!ContentTyape ? <p>Нажми на кнопку</p> : null}
+          {ContentTyape ? <p>{differences[ContentTyape]}</p> : null} */}
+
+          {!ContentTyape && <p>Нажми на кнопку</p>}
+          {ContentTyape && <p>{differences[ContentTyape]}</p>}
+
+          
         </section>
       </main>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import logo from '/logo-name.svg'
 
 export default function Header(){
-    const now = new Date()
+  const [ now, setNow ] = useState(new Date())
+  setInterval(() => setNow(new Date()), 1000)
     return (
       <header>
         <img src={logo} alt="result"/>
-        {/* <h3>Shpyra Univercity</h3> */}
+        
         <span>Время сейчас: {now.toLocaleTimeString()}</span>
       </header>
     )
